6e0461f1c48f251ac5921cf6152925a99c7f32f5
ahmadf249 <ahmadfadhli249@gmail.com>
2024-01-15 10:54:00 +0700

    add autoincrement on models id

--- models/articleModel.go
+++ models/articleModel.go
@@ -8,7 +8,7 @@ import (
 
 type Article struct {
 	gorm.Model
-	ID            int       `json:"id"`
+	ID            int       `json:"id" sql:"AUTO_INCREMENT" gorm:"primary_key"`
 	Title         string    `json:"title"`
 	Content       string    `json:"content"`
 	PublishedDate time.Time `json:"published_date"`
--- models/videoModel.go
+++ models/videoModel.go
@@ -4,7 +4,7 @@ import "gorm.io/gorm"
 
 type Video struct {
 	gorm.Model
-	ID          int    `json:"id"`
+	ID          int    `json:"id" sql:"AUTO_INCREMENT" gorm:"primary_key"`
 	Title       string `json:"title"`
 	Description string `json:"description"`
 	URL         string `json:"url"`
